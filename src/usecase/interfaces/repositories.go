f483ddb1b216bcbf98f3539d530ae5126a267b9d
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-07-17 21:14:21 +0900

    create interfaces

---
+++ src/usecase/interfaces/repositories.go
@@ -0,0 +1,9 @@
+package interfaces
+
+import "github.com/katsukiniwa/practical-go-programming/src/domain"
+
+type UserRepository interface {
+	Store(domain.User) (int, error)
+	FindByName(string) ([]domain.User, error)
+	FindAll() ([]domain.User, error)
+}
