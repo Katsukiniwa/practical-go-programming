f4334e3518bd82f5735b21045cbaf75b8e5e19d4
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-07-20 20:54:07 +0900

    create user repository

--- src/usecase/interfaces/repositories.go
+++ src/usecase/interfaces/repositories.go
@@ -1,9 +1,11 @@
 package interfaces
 
-import "github.com/katsukiniwa/practical-go-programming/src/domain"
+import (
+	"github.com/katsukiniwa/practical-go-programming/src/domain/model"
+)
 
 type UserRepository interface {
-	Store(domain.User) (int, error)
-	FindByName(string) ([]domain.User, error)
-	FindAll() ([]domain.User, error)
+	Store(model.User) (int, error)
+	FindByName(string) ([]model.User, error)
+	FindAll() ([]model.User, error)
 }
