794696168570f46bfb57dbdd5734356b798caffa
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-08-02 07:57:37 +0900

    create slice sum

--- playground/sum.go
+++ playground/sum.go
@@ -1,9 +1,17 @@
 package main
 
-func Sum(numbers [5]int) int {
+func SumArray(numbers [5]int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func SumSlice(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
 }
