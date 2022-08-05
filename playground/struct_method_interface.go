cb664f470a8773d465c47ea9baec889a6259532b
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-08-05 20:25:52 +0900

    create area method

--- playground/struct_method_interface.go
+++ playground/struct_method_interface.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"math"
+)
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
@@ -11,4 +15,16 @@ func Area(rectangle Rectangle) float64 {
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height 
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
