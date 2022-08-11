7cb73d6f2ec9c1c111ab9bd0b892a92cc2688fc7
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-08-11 21:08:23 +0900

    create http server

--- go.mod
+++ go.mod
@@ -2,12 +2,4 @@ module github.com/katsukiniwa/practical-go-programming
 
 go 1.17
 
-require (
-	github.com/google/wire v0.5.0
-	github.com/jinzhu/gorm v1.9.16
-)
-
-require (
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	golang.org/x/tools v0.0.0-20190422233926-fe54fb35175b // indirect
-)
+require github.com/google/wire v0.5.0
--- server.go
+++ server.go
@@ -2,40 +2,71 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
-	"log"
-	"os"
-
-	"github.com/katsukiniwa/practical-go-programming/bar"
-	"github.com/katsukiniwa/practical-go-programming/baz"
+	"math/rand"
+	"net/http"
+	"sync"
+	"time"
 )
 
-type ip struct {
-	Origin string `json:"origin"`
-	URL    string `json:"url"`
-}
-
-func ipFunc() {
-	f, err := os.Open("ip.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-
-	var resp ip
-	if err := json.NewDecoder(f).Decode(&resp); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%+v\n", resp)
+type Comment struct {
+	Message  string
+	UserName string
 }
 
 func main() {
-	fmt.Print("Hello World")
-	ipFunc()
-	bar.Sample()
-	bar.DecodeIp()
-	bar.PrintUser()
-	bar.PrintFormInput()
-	bar.PrintBottle()
-	baz.Run()
+	var appPort int
+	flag.IntVar(&appPort, "port", 9000, "port")
+	flag.Parse()
+
+	rand.Seed(time.Now().UnixNano())
+
+	fmt.Printf("Server is Ready on :%d", appPort)
+
+	var mutex = &sync.RWMutex{}
+	comments := make([]Comment, 0, 100)
+
+	http.HandleFunc("/comments", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		switch r.Method {
+		case http.MethodGet:
+			/*
+			* 読み寄り時に書き込みかあることを考慮しロックする
+			* 本来はDBから読み取る処理を代替
+			 */
+			mutex.RLock()
+
+			if err := json.NewEncoder(w).Encode(comments); err != nil {
+				http.Error(w, fmt.Sprintf(`{"status:"%s"}`, err), http.StatusInternalServerError)
+				return
+			}
+
+			mutex.RUnlock()
+
+		case http.MethodPost:
+			var c Comment
+			if err := json.NewDecoder(r.Body).Decode(&c); err != nil {
+				http.Error(w, fmt.Sprintf(`{"status":"%s"}`, err), http.StatusInternalServerError)
+				return
+			}
+
+			mutex.Lock()
+			// comments := append(comments, c)
+			mutex.Unlock()
+
+			w.WriteHeader(http.StatusCreated)
+			w.Write([]byte(`{"status": "created"}`))
+
+		default:
+			http.Error(w, `{"status":"permits only GET or POST"`, http.StatusInternalServerError)
+		}
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World")
+	})
+
+	http.ListenAndServe(":9000", nil)
 }
