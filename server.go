58f7f0dad939258ef9bda4357f22eebc10f17c42
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-08-13 18:41:03 +0900

    create middleware

--- server.go
+++ server.go
@@ -6,7 +6,15 @@ import (
 	"math/rand"
 	"net/http"
 	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/katsukiniwa/practical-go-programming/pkg/gateway/middleware"
 )
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
 
 func main() {
 	var appPort int
@@ -17,9 +25,32 @@ func main() {
 
 	fmt.Printf("Server is Ready on :%d\n", appPort)
 
+	yes := 0
+	no := 0
+
+	r := chi.NewRouter()
+
+	/*
+	 * answer handler
+	 */
+	r.Post("/poll/{answer}", func(w http.ResponseWriter, r *http.Request) {
+		if chi.URLParam(r, "answer") == "y" {
+			yes++
+		} else {
+			no++
+		}
+	})
+
+	r.Get("/result", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "賛成: %d, 反対: %d", yes, no)
+	})
+	r.Handle("/asset/*", http.StripPrefix("/asset/", http.FileServer(http.Dir("."))))
+
 	http.HandleFunc("/", RootHandler)
 
-	http.HandleFunc("/params", handleParams)
+	http.HandleFunc("/params", ParamsHandler)
+
+	http.Handle("health", http.HandlerFunc(middleware.HealthCheck))
 
 	http.ListenAndServe(":9000", nil)
 }
@@ -28,7 +59,7 @@ func RootHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "hello world")
 }
 
-func handleParams(w http.ResponseWriter, r *http.Request) {
+func ParamsHandler(w http.ResponseWriter, r *http.Request) {
 
 	// クエリパラメータ取得してみる
 	fmt.Fprintf(w, "クエリ：%s\n", r.URL.RawQuery)
