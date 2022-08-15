9755f9309e6ad0dae41dd653c1ab808f875a0f56
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-08-16 08:04:35 +0900

    add assertStrings

--- playground/dicitionary_test.go
+++ playground/dicitionary_test.go
@@ -4,13 +4,19 @@ import (
 	"testing"
 )
 
+func assertStrings(t *testing.T, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
 func TestSearch(t *testing.T) {
 	dictionary := map[string]string{"test": "this is just a test"}
 
 	got := Search(dictionary, "test")
 	want := "this is just a test"
 
-	if got != want {
-		t.Errorf("got %q want %q given %q", got, want, "test")
-	}
+	assertStrings(t, got, want)
 }
