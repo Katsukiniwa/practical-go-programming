111830ba2b9a7d1c7c71b6efe58e4bf4176d3df2
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-08-15 23:49:14 +0900

    assert no error

--- playground/pointer_and_error_test.go
+++ playground/pointer_and_error_test.go
@@ -24,6 +24,13 @@ func assertError(t *testing.T, got error, want error) {
 	}
 }
 
+func assertNoError(t *testing.T, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
+	}
+}
+
 func TestWallet(t *testing.T) {
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
@@ -36,9 +43,10 @@ func TestWallet(t *testing.T) {
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(30)}
 
-		wallet.Withdraw(Bitcoin(20))
+		err := wallet.Withdraw(Bitcoin(20))
 
 		assertBalance(t, wallet, Bitcoin(10))
+		assertNoError(t, err)
 	})
 
 	t.Run("Withdraw insufficient funds", func(t *testing.T) {
