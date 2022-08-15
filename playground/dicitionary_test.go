c681a16b4b44408ef3c21845e798ad6e117964c3
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-08-16 07:57:42 +0900

    [ci skip] add test

--- playground/dicitionary_test.go
+++ playground/dicitionary_test.go
@@ -1,1 +1,16 @@
 package playground
+
+import (
+	"testing"
+)
+
+func TestSearch(t *testing.T) {
+	dictionary := map[string]string{"test": "this is just a test"}
+
+	got := Search(dictionary, "teat")
+	want := "this is just a test"
+
+	if got != want {
+		t.Errorf("got %q want %q given", got, want, "test")
+	}
+}
